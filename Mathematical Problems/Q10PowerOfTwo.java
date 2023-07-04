c100725473713e6529c30343ef5a3d197dc4f5b1
Akshita Sharma <84262839+Akshita5402@users.noreply.github.com>
2023-07-05 00:42:50 +0530

    Update Q10PowerOfTwo.java

--- Mathematical Problems/Q10PowerOfTwo.java
+++ Mathematical Problems/Q10PowerOfTwo.java
@@ -1,1 +1,20 @@
-Mathematical Problems
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+       
+        if(n==1){return true;}
+        if(n==0){return false;}
+        
+
+       while(n!=1){
+           if(n%2!=0 || n<0){
+               return false;
+
+           }
+
+           n=n/2;
+//no is divided by 2 and it is divided by 2 again and again to check if it is the power of 2
+
+       }
+       return true;
+    }
+}
