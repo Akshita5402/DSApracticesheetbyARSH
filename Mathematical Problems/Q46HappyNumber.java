adcb584d9443fa5cb13a1e07a25a471cda1d808b
Akshita Sharma <84262839+Akshita5402@users.noreply.github.com>
2023-07-17 23:53:45 +0530

    DAY 16

---
+++ Mathematical Problems/Q46HappyNumber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        while(true){
+            int sum=0;
+            while(n!=0){
+                sum+=(n%10)*(n%10);
+                n=n/10;}
+                if(sum/10==0){
+                if(sum==1||sum==7){
+                    return true;
+                }
+                else{ return false;}
+                }
+
+            
+            n=sum;
+        }
+        
+      //  return true;
+
+        
+    }
+}
